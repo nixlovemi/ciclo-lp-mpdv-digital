d42e4b4acd3ae9ff6e92a36747daa39b7c3c0bbb
Leandro Parra <nixlovemi@gmail.com>
2023-06-29 13:37:54 -0400

    part 1 - intro container

---
+++ work/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <title>MPDV Digital | Ciclo Comunicação Criativa</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    <body>
+        <div class="row no-gutters bg-yellow">
+            <div id="header-intro-container" class="col">
+                <div class="container pt-5">
+                    <div class="header-intro-text font-barlow">
+                        CHEGOU A HORA DE IMPACTAR
+                    </div>
+                    <br />
+                    <div class="header-intro-text font-barlow-bold">
+                        O PONTO DE VENDA COM O MPDV
+                    </div>
+                    <br />
+                    <div class="header-intro-text-digital text-white font-hackney-vector">
+                        DIGITAL
+                    </div>
+                </div>
+
+                <img id="arte-header" class="img-fluid" src="img/arte-top-header.gif" />
+            </div>
+        </div>
+
+        <div id="container-lancamento" class="container mt-5 mb-5">
+            <div class="cl-text font-barlow pt-4">
+                UM LANÇAMENTO QUE CHEGOU
+            </div>
+            <br />
+            <div class="cl-text container-lancamento font-barlow-bold mb-3">
+                PARA INOVAR O VAREJO BRASILEIRO!
+            </div>
+
+            <div id="cl-descricao" class="font-barlow-semi">
+                A <strong>Ciclo Comunicação Criativa</strong> encontrou um jeito de inovar a maneira de chamar
+                atenção para as gôndolas, através do <strong>MPDV Digital</strong>. Uma solução de sinalização
+                exclusiva que só a gente pode oferecer no Brasil, destacando <strong>sua marca</strong> nas
+                gôndulas e <strong>aumentando sua performance na loja</strong>.
+            </div>
+        </div>
+
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+    </body>
+</html>
